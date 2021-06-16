77bd0e41d23dba9c8fa47e82709f8b84e7d03d72
Michael DeSantis <m.desantis628@gmail.com>
2021-06-16 19:46:44 -0400

    commit on main

--- rockspaperscissor.js
+++ rockspaperscissor.js
@@ -11,19 +11,43 @@ function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection){
   return "tie, play again"
   }else if (playerSelection == 'rock' && computerSelection == 'scissors'){
+    playerScore++
   return "you win"
   }else if (playerSelection == 'paper' && computerSelection == 'rock'){
+    playerScore++
     return "you win"
   }else if (playerSelection == 'scissors' && computerSelection == 'paper'){
+    playerScore++
     return "you win"
-  }else {
+  }else if{
+    computerScore++
     return "you lose"
+  } else return "Invalid Entry"
+}
+
+function game() {
+  while (playerScore <= 5 && computerScore <= 5) {
+    let playerSelection = prompt(' Enter Rock, Paper, or Scissors');
+    let computerSelection = computerPlay();
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    alert(playRound(playerSelection, computerSelection));
+  }
+  alert(winner());
+}
+
+function winner(){
+  if (playerScore == 5){
+    return "You won the game!"
+  }  
+  if (computerScore == 5) 
+    return "Computer wins the game!"
   }
 }
 
-const playerSelection = "rock";
+//const playerSelection = "rock";
 const computerSelection = computerPlay();
 
 console.log(playRound(playerSelection, computerSelection));
-
-
+console.log(playerScore)
+console.log(computerScore)
