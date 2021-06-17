ecd13c32993ed4a7653d132594feab7c5c69dbbe
Michael DeSantis <m.desantis628@gmail.com>
2021-06-16 21:04:31 -0400

    commit on main

--- rockspaperscissor.js
+++ rockspaperscissor.js
@@ -2,52 +2,48 @@ let choices = ['rock', 'paper', 'scissors'];
 let playerScore = 0;
 let computerScore = 0;
 
+
 function computerPlay(){
   let num = choices [Math.floor(Math.random() * choices.length)]
   return num
 }
+
+//game()
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection === computerSelection){
   return "tie, play again"
   }else if (playerSelection == 'rock' && computerSelection == 'scissors'){
     playerScore++
-  return "you win"
+    return "you win"
   }else if (playerSelection == 'paper' && computerSelection == 'rock'){
     playerScore++
     return "you win"
   }else if (playerSelection == 'scissors' && computerSelection == 'paper'){
     playerScore++
     return "you win"
-  }else if{
+  }else if (playerSelection !== 'rock'|| 'paper'|| 'scissors'){
+    return "Invalid Entry"
+  }else
     computerScore++
     return "you lose"
-  } else return "Invalid Entry"
 }
 
-function game() {
-  while (playerScore <= 5 && computerScore <= 5) {
-    let playerSelection = prompt(' Enter Rock, Paper, or Scissors');
-    let computerSelection = computerPlay();
-    playerSelection = playerSelection.toLowerCase();
-    computerSelection = computerSelection.toLowerCase();
-    alert(playRound(playerSelection, computerSelection));
-  }
-  alert(winner());
-}
 
-function winner(){
-  if (playerScore == 5){
-    return "You won the game!"
-  }  
-  if (computerScore == 5) 
-    return "Computer wins the game!"
-  }
-}
+
+//function game()
+  
+
+let playerSelections = prompt(' Choose Rock, Paper, or Scissors');
+let computerSelections = computerPlay();
+let playerSelection = playerSelections.toLowerCase();
+let computerSelection = computerSelections.toLowerCase();
 
 //const playerSelection = "rock";
-const computerSelection = computerPlay();
-
+//const computerSelection = computerPlay();
+    
 console.log(playRound(playerSelection, computerSelection));
 console.log(playerScore)
 console.log(computerScore)
+console.log(playerSelection)
+console.log(computerSelection)
