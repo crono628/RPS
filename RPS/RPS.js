de490994ae85214410f014dabef71fc516317be6
Michael DeSantis <m.desantis628@gmail.com>
2021-07-02 17:59:24 -0400

    commit on rps-ui

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -1,58 +1,67 @@
-let choices = ['rock', 'paper', 'scissors'];
-let human = 'you win'
-let computer = 'computer wins'
-let tie = 'tie, play again'
+let choices = ["rock", "paper", "scissors"];
+let human = "you win";
+let computer = "computer wins";
+let tie = "tie, play again";
 
-function computerPlay(){
-  let num = choices [Math.floor(Math.random() * 3)]
-  return num
-} 
-
-
-function playRound(playerSelection, computerSelection){
-
-
+function computerPlay() {
+  let num = choices[Math.floor(Math.random() * 3)];
+  return num;
 }
 
-
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    round++;
+    return tie;
+  }
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    playerScore++;
+    round++;
+    return human;
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    playerScore++;
+    round++;
+    return human;
+  }
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    playerScore++;
+    round++;
+    return human;
+  } else {
+    computerScore++;
+    round++;
+    return computer;
+  }
+}
 
 let playerScore = 0;
 let computerScore = 0;
+let round = 0;
 
-const buttons = document.querySelectorAll('button');
+let game = () => {
+  let playerSelection = document.querySelectorAll("button");
+  playerSelection.forEach(function (userItem) {
+    return userItem;
+  });
 
+  const computerSelection = computerPlay();
 
+  console.log(
+    `round ${round} - ${playRound(playerSelection, computerSelection)} `
+  );
+  console.log(`you chose ${playerSelection}, score is ${playerScore}`);
+  console.log(`computer chose ${computerSelection}, score ${computerScore}`);
+  console.log("");
 
-function play(playerSelection) {
-    const computerSelection = computerPlay()
-    if (playerSelection === computerSelection){
-        return tie;
+  if (playerScore !== 5) {
+    if (computerScore !== 5) {
+      game();
+    } else {
+      console.log("game over, the computer wins the series");
     }
-    if (playerSelection === 'rock' && computerSelection === 'scissors'){
-        playerScore++
-        return human;
-    }
-    if (playerSelection === 'paper' && computerSelection === 'rock'){
-        playerScore++
-        return human; 
-    }
-    if (playerSelection === 'scissors'&& computerSelection === 'paper'){
-        playerScore++
-        return human;
-    }
-    else {
-        computerScore++
-        return computer;
-    }
+  } else {
+    console.log("game over, you win the series");
+  }
+};
 
-    
-}
-buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        return(button.id)
-}
-
-play();
-
-
-console.log(play())
+game();
