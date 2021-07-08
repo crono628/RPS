5c2f6d6b6c0e11f67b73c3acc1f7683b712cfae3
Michael DeSantis <m.desantis628@gmail.com>
2021-07-07 20:56:45 -0400

    commit on rps-ui

--- RPS/RPS.js
+++ RPS/RPS.js
@@ -1,6 +1,6 @@
 let choices = ["rock", "paper", "scissors"];
-let human = "you win";
-let computer = "computer wins";
+let human = () => ("you win", playerScore++);
+let computer = () => ("computer wins", computerScore++);
 let tie = "tie, play again";
 let playerScore = 0;
 let computerScore = 0;
@@ -11,74 +11,85 @@ document.getElementById("paper").onclick = game;
 document.getElementById("scissors").onclick = game;
 
 function computerPlay() {
-  let num = choices[Math.floor(Math.random() * 3)];
-  return num;
+    let num = choices[Math.floor(Math.random() * 3)];
+    return num;
 }
 
 function game() {
-  let playerSelection = this.id;
+    let playerSelection = this.id;
+    const computerSelection = computerPlay();
+    playRound(playerSelection, computerSelection);
 
-  const computerSelection = computerPlay();
+    function playRound(playerChoice, computerChoice) {
+        if (playerChoice === computerChoice) {
+            round++;
+            checkScore();
+            endSeries();
+            return tie;
+        }
+        if (playerChoice === "rock" && computerChoice === "scissors") {
+            playerScore++;
+            round++;
+            checkScore();
+            endSeries();
+            return human;
+        }
+        if (playerChoice === "paper" && computerChoice === "rock") {
+            playerScore++;
+            round++;
+            checkScore();
+            endSeries();
+            return human;
+        }
+        if (playerChoice === "scissors" && computerChoice === "paper") {
+            playerScore++;
+            round++;
+            checkScore();
+            endSeries();
+            return human;
+        } else {
+            computerScore++;
+            round++;
+            checkScore();
+            endSeries();
+            return computer;
+        }
+    }
+    console.log(playerScore)
+    console.log(computerScore)
+}
 
-  function playRound(playerChoice, computerChoice) {
-    if (playerChoice === computerChoice) {
-      round++;
-      checkScore();
-      endSeries();
-      return tie;
-    }
-    if (playerChoice === "rock" && computerChoice === "scissors") {
-      playerScore++;
-      round++;
-      checkScore();
-      endSeries();
-      return human;
-    }
-    if (playerChoice === "paper" && computerChoice === "rock") {
-      playerScore++;
-      round++;
-      checkScore();
-      endSeries();
-      return human;
-    }
-    if (playerChoice === "scissors" && computerChoice === "paper") {
-      playerScore++;
-      round++;
-      checkScore();
-      endSeries();
-      return human;
-    } else {
-      computerScore++;
-      round++;
-      checkScore();
-      endSeries();
-      return computer;
-    }
-  }
-
-  function endSeries() {
+function endSeries() {
     if (playerScore === 5) {
-      console.log("YOU WIN THE SERIES");
+        console.log("YOU WIN THE SERIES");
     }
     if (computerScore === 5) {
-      console.log("YOU LOSE THE SERIES");
+        console.log("YOU LOSE THE SERIES");
     }
-    console.log(playerScore);
-    console.log(computerScore);
-  }
+    return false
+}
 
-  let checkScore = () =>
+let checkScore = () =>
     playerScore === 5 ? true : computerScore === 5 ? true : false;
 
-  function reset() {
+function reset() {
     if (checkScore()) {
-      playerScore = 0;
-      computerScore = 0;
+        playerScore = 0;
+        computerScore = 0;
     }
-  }
-  reset();
-  console.log("player: " + playerSelection);
-  console.log("computer: " + computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
-  console.log(" ---------------- ");
+}
+
+function gameString() {
+    let win = document.querySelector("#gameoverString");
+    if (playerScore === 5) {
+        win.textContent = "YOU ARE THE CHAMPION";
+        reset();
+        return true;
+    }
+    if (computerScore === 5) {
+        win.textContent = "COMPUTER IS THE CHAMPION"
+        reset();
+        return true;
+    }
+    return false;
 }
