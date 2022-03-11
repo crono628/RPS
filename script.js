35933e06c2fb75bbc7f473969f23c286f082018a
crono628 <84817352+crono628@users.noreply.github.com>
2022-03-10 20:52:28 -0500

    logic functioning. 100%.

--- script.js
+++ script.js
@@ -6,67 +6,95 @@ const playerResults = document.querySelector('.player-results');
 const computerResults = document.querySelector('.computer-results');
 const playerScoreboard = document.querySelector('.player-scoreboard');
 const computerScoreboard = document.querySelector('.computer-scoreboard');
+const resetButton = document.querySelector('.reset-button');
+const finalResult = document.querySelector('.tie-results');
 
 let playerScore = 0;
 let computerScore = 0;
 let playerWinRound = null;
-let history = [];
+let gameOn = true;
 
+//add event listeners for selections and reset button
 selections.forEach((selection) => {
   selection.addEventListener('click', (e) => {
     game(e.target.className, computerMove());
   });
 });
 
+resetButton.addEventListener('click', reset);
+
+//produce random player selection
 function computerMove() {
   let randomNum = Math.floor(Math.random() * 3);
   let choices = ['rock', 'paper', 'scissors'];
   return choices[randomNum];
 }
 
-function showHistory() {
-  renderResults(playerResults);
-  renderResults(computerResults);
-  history.forEach((item) => {
-    let newDiv = document.createElement('div');
-    newDiv.textContent = item.player;
-    playerResults.appendChild(newDiv);
-  });
-  history.forEach((item) => {
-    let newDiv = document.createElement('div');
-    newDiv.textContent = item.computer;
-    computerResults.appendChild(newDiv);
-  });
-}
-
-function renderResults(parent) {
-  while (parent.firstChild) {
-    parent.removeChild(parent.firstChild);
+//end game logic
+function determineWinner() {
+  if (playerScore === 3) {
+    finalResult.textContent = 'You win!';
+    gameOn = false;
   }
-}
-
-function game(player, computer) {
-  playerWinRound = null;
-  history.unshift({ player: player, computer: computer });
-  if (player === computer) {
-    return;
-  } else if (
-    (player === 'rock' && computer === 'scissors') ||
-    (player === 'scissors' && computer === 'paper') ||
-    (player === 'paper' && computer === 'rock')
-  ) {
-    playerScore++;
-    playerScoreboard.textContent = `Player: ${playerScore}`;
-    playerWinRound = true;
+  if (computerScore === 3) {
+    finalResult.textContent = 'Computer wins!';
+    gameOn = false;
+  }
+  if (gameOn) {
+    resetButton.classList.add('game-on');
   } else {
-    computerScore++;
-    computerScoreboard.textContent = `Player: ${computerScore}`;
-    playerWinRound = false;
+    resetButton.classList.remove('game-on');
   }
-  showHistory();
-  playerWinRound === true
-    ? playerResults.firstChild.classList.add('winner')
-    : playerWinRound === false
-    ? computerResults.firstChild.classList.add('winner')
-    : null;
+}
+determineWinner();
+
+//things the need resetting to begin a new game
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  computerScoreboard.textContent = `Computer: ${computerScore}`;
+  playerScoreboard.textContent = `Player: ${playerScore}`;
+  playerWinRound = null;
+  gameOn = true;
+  playerResults.textContent = '';
+  computerResults.textContent = '';
+  finalResult.textContent = '';
+  determineWinner();
+}
+
+//game logic
+function game(player, computer) {
+  if (!gameOn) {
+    return;
+  } else {
+    playerResults.textContent = player;
+    computerResults.textContent = computer;
+    playerWinRound = null;
+    if (player === computer) {
+      document.querySelector('.tie-results').textContent = 'Tie!';
+      playerResults.classList.remove('winner');
+      computerResults.classList.remove('winner');
+      return;
+    } else if (
+      (player === 'rock' && computer === 'scissors') ||
+      (player === 'scissors' && computer === 'paper') ||
+      (player === 'paper' && computer === 'rock')
+    ) {
+      document.querySelector('.tie-results').textContent = '';
+      playerScore++;
+      playerScoreboard.textContent = `Player: ${playerScore}`;
+      playerWinRound = true;
+      playerResults.classList.add('winner');
+      computerResults.classList.remove('winner');
+      determineWinner();
+    } else {
+      document.querySelector('.tie-results').textContent = '';
+      computerScore++;
+      computerScoreboard.textContent = `Player: ${computerScore}`;
+      playerWinRound = false;
+      computerResults.classList.add('winner');
+      playerResults.classList.remove('winner');
+      determineWinner();
+    }
+  }
 }
